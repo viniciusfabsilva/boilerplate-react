20e5d2ef70988b1394b8d990a6551213acdd15cd
vinicius <viniciusfabi1@hotmail.com>
2024-09-26 21:27:53 -0300

    Add CI workflow and configuration plop

---
+++ .jest/setup.ts
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom'
+import 'jest-styled-components'
+
+import React from 'react';
+
+global.React = React;
---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+    staticDirs: ['../public'],
+    stories: ['../src/components/**/stories.tsx'],
+    addons: [
+        '@storybook/addon-essentials'
+    ],
+    framework: {
+        name: '@storybook/nextjs',
+        options: {}
+    },
+    docs: {
+        autodocs: true
+    },
+    webpackFinal: (config) => {
+        config.resolve.modules.push(`${process.cwd()}/src`)
+        return config
+    }
+}
+export default config
